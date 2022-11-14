0b037a2544e11e0afc64a5dcac1e0053d4b82078
salah <salahwaled368@gmail.com>
2022-11-14 15:02:55 +0300

    Bassam5

---
+++ lib/controller/data_Controller.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+import 'package:task_management/services/services.dart';
+
+class DataController extends GetxController {
+  DataServicce service = DataServicce();
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+  List<dynamic> _myData = [];
+
+  List<dynamic> get myData => _myData;
+
+  Future<void> getData() async {
+    _isLoading = true;
+    Response response = await service.getData();
+    if (response.statusCode == 200) {
+      _myData = response.body;
+      print("basssssssssssam 1");
+      update();
+    } else {
+      print("basssssssssssam 2");
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:task_management/Screen/add_task.dart';
 import 'package:task_management/Screen/all_task_screen.dart';
+import 'package:task_management/controller/data_Controller.dart';
 import 'package:task_management/Screen/home_screen.dart';
 
 void main() {
@@ -10,10 +11,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  loadData() async{
+    await Get.find<DataController>().getData();
+  }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Get.lazyPut(() => DataController());
+    loadData();
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
